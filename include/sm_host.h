4bd40b20e4ba77b1748f0874d5edce7bb66c1863
Tommi Rantanen <tommi.rantanen@nordicsemi.no>
2025-09-19 15:49:00 +0300

    lib: sm_host: Rename SLM to SM in code
    Rename occurrences of slm/SLM to sm/SM/Serial Modem in
    SM Host library and SM Host Shell sample.
    Modem SLM is replaced with SM Host.
    SLM Shell is renamed to SM Host Shell.
    
    Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>

--- include/sm_host.h
+++ include/sm_host.h
@@ -4,17 +4,17 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
  */
 
-#ifndef MODEM_SLM_H_
-#define MODEM_SLM_H_
+#ifndef SM_HOST_H_
+#define SM_HOST_H_
 
 /**
- * @file modem_slm.h
+ * @file sm_host.h
  *
- * @defgroup modem_slm Modem SLM library
+ * @defgroup sm_host Serial Modem Host library
  *
  * @{
  *
- * @brief Public APIs for the Modem SLM library.
+ * @brief Public APIs for the Serial Modem Host library.
  */
 
 #ifdef __cplusplus
@@ -29,7 +29,7 @@ extern "C" {
 #include <zephyr/toolchain.h>
 
 /** Max size of AT command response is 2100 bytes. */
-#define SLM_AT_CMD_RESPONSE_MAX_LEN 2100
+#define SM_AT_CMD_RESPONSE_MAX_LEN 2100
 
 /**
  * @brief AT command result codes
@@ -43,66 +43,65 @@ enum at_cmd_state {
 };
 
 /**
- * @typedef slm_data_handler_t
+ * @typedef sm_data_handler_t
  *
- * Handler to handle data received from SLM, which could be AT response, AT notification
+ * Handler to handle data received from Serial Modem, which could be AT response, AT notification
  * or simply raw data (for example DFU image).
  *
- * @param data    Data received from SLM.
+ * @param data    Data received from Serial Modem.
  * @param datalen Length of the data received.
  *
- * @note The handler runs from uart callback. It must not call @ref modem_slm_send_cmd. The data
+ * @note The handler runs from uart callback. It must not call @ref sm_host_send_cmd. The data
  * should be copied out by the application as soon as called.
  */
-typedef void (*slm_data_handler_t)(const uint8_t *data, size_t datalen);
+typedef void (*sm_data_handler_t)(const uint8_t *data, size_t datalen);
 
 /**
- * @typedef slm_ind_handler_t
+ * @typedef sm_ind_handler_t
  *
- * Handler to handle @kconfig{CONFIG_MODEM_SLM_INDICATE_PIN} signal from SLM.
+ * Handler to handle @kconfig{CONFIG_SM_HOST_INDICATE_PIN} signal from Serial Modem.
  */
-typedef void (*slm_ind_handler_t)(void);
+typedef void (*sm_ind_handler_t)(void);
 
-/**@brief Initialize Modem SLM library.
+/**@brief Initialize Serial Modem Host library.
  *
- * @param handler Pointer to a handler function of type @ref slm_data_handler_t.
+ * @param handler Pointer to a handler function of type @ref sm_data_handler_t.
  *
  * @return Zero on success, non-zero otherwise.
  */
-int modem_slm_init(slm_data_handler_t handler);
+int sm_host_init(sm_data_handler_t handler);
 
-/**@brief Un-initialize Modem SLM library.
- *
+/**@brief Un-initialize Serial Modem Host
  */
-int modem_slm_uninit(void);
+int sm_host_uninit(void);
 
 /**
- * @brief Register callback for @kconfig{CONFIG_MODEM_SLM_INDICATE_PIN} indication
+ * @brief Register callback for @kconfig{CONFIG_SM_HOST_INDICATE_PIN} indication
  *
- * @param handler Pointer to a handler function of type @ref slm_ind_handler_t.
+ * @param handler Pointer to a handler function of type @ref sm_ind_handler_t.
  * @param wakeup  Enable/disable System Off wakeup by GPIO Sense.
  *
  * @retval Zero    Success.
- * @retval -EFAULT if @kconfig{CONFIG_MODEM_SLM_INDICATE_PIN} is not defined.
+ * @retval -EFAULT if @kconfig{CONFIG_SM_HOST_INDICATE_PIN} is not defined.
  */
-int modem_slm_register_ind(slm_ind_handler_t handler, bool wakeup);
+int sm_host_register_ind(sm_ind_handler_t handler, bool wakeup);
 
 /**
  * @brief Toggle power pin of the nRF91 Series device configured with
- * @kconfig{CONFIG_MODEM_SLM_POWER_PIN}.
+ * @kconfig{CONFIG_SM_HOST_POWER_PIN}.
  *
- * The pin is enabled for the time specified in @kconfig{CONFIG_MODEM_SLM_POWER_PIN_TIME}
+ * The pin is enabled for the time specified in @kconfig{CONFIG_SM_HOST_POWER_PIN_TIME}
  * and then disabled.
  *
  * @return Zero on success, non-zero otherwise.
  */
-int modem_slm_power_pin_toggle(void);
+int sm_host_power_pin_toggle(void);
 
 /**
- * @brief Function to send an AT command in SLM command mode
+ * @brief Function to send an AT command in Serial Modem command mode
  *
  * This function wait until command result is received. The response of the AT command is received
- * via the @ref slm_ind_handler_t registered in @ref modem_slm_init.
+ * via the @ref sm_ind_handler_t registered in @ref sm_host_init.
  *
  * @param command Pointer to null terminated AT command string without command terminator
  * @param timeout Response timeout for the command in seconds, Zero means infinite wait
@@ -111,33 +110,33 @@ int modem_slm_power_pin_toggle(void);
  * @retval -EAGAIN if command execution times out.
  * @retval other if command execution fails.
  */
-int modem_slm_send_cmd(const char *const command, uint32_t timeout);
+int sm_host_send_cmd(const char *const command, uint32_t timeout);
 
 /**
- * @brief Function to send raw data in SLM data mode
+ * @brief Function to send raw data in Serial Modem data mode
  *
  * @param data    Raw data to send
  * @param datalen Length of the raw data
  *
  * @return Zero on success, non-zero otherwise.
  */
-int modem_slm_send_data(const uint8_t *const data, size_t datalen);
+int sm_host_send_data(const uint8_t *const data, size_t datalen);
 
 /**
- * @brief SLM monitor callback.
+ * @brief Serial Modem monitor callback.
  *
  * @param notif The AT notification callback.
  */
-typedef void (*slm_monitor_handler_t)(const char *notif);
+typedef void (*sm_monitor_handler_t)(const char *notif);
 
 /**
- * @brief SLM monitor entry.
+ * @brief Serial Modem monitor entry.
  */
-struct slm_monitor_entry {
+struct sm_monitor_entry {
 	/** The filter for this monitor. */
 	const char *filter;
 	/** Monitor callback. */
-	const slm_monitor_handler_t handler;
+	const sm_monitor_handler_t handler;
 	/** Monitor is paused. */
 	uint8_t paused;
 };
@@ -150,7 +149,7 @@ struct slm_monitor_entry {
 #define MON_ACTIVE 0
 
 /**
- * @brief Define an SLM monitor to receive notifications in the system workqueue thread.
+ * @brief Define an Serial Modem monitor to receive notifications in the system workqueue thread.
  *
  * @param name The monitor's name.
  * @param _filter The filter for AT notification the monitor should receive,
@@ -159,9 +158,9 @@ struct slm_monitor_entry {
  * @param ... Optional monitor initial state (@c MON_PAUSED or @c MON_ACTIVE).
  *	      The default initial state of a monitor is @c MON_ACTIVE.
  */
-#define SLM_MONITOR(name, _filter, _handler, ...)                                                  \
+#define SM_MONITOR(name, _filter, _handler, ...)                                                  \
 	static void _handler(const char *);                                                        \
-	static STRUCT_SECTION_ITERABLE(slm_monitor_entry, name) = {                                \
+	static STRUCT_SECTION_ITERABLE(sm_monitor_entry, name) = {                                \
 		.filter = _filter,                                                                 \
 		.handler = _handler,                                                               \
 		COND_CODE_1(__VA_ARGS__, (.paused = __VA_ARGS__,), ())                             \
@@ -174,7 +173,7 @@ struct slm_monitor_entry {
  *
  * @param mon The monitor to pause.
  */
-static inline void slm_monitor_pause(struct slm_monitor_entry *mon)
+static inline void sm_monitor_pause(struct sm_monitor_entry *mon)
 {
 	mon->paused = MON_PAUSED;
 }
@@ -186,7 +185,7 @@ static inline void slm_monitor_pause(struct slm_monitor_entry *mon)
  *
  * @param mon The monitor to resume.
  */
-static inline void slm_monitor_resume(struct slm_monitor_entry *mon)
+static inline void sm_monitor_resume(struct sm_monitor_entry *mon)
 {
 	mon->paused = MON_ACTIVE;
 }
@@ -197,4 +196,4 @@ static inline void slm_monitor_resume(struct slm_monitor_entry *mon)
 
 /** @} */
 
-#endif /* MODEM_SLM_H_ */
+#endif /* SM_HOST_H_ */
