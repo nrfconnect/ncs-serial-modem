b487e766d123a4f5a66f83506bf8365bc659ea3c
Seppo Takalo <seppo.takalo@nordicsemi.no>
2025-11-17 11:04:52 +0200

    app: scripts: Wait for connection termination
    When shutdown script is called, wait for connection
    to terminate.
    
    Similarly wait for PPP to start the interface before
    returning from the start up script.
    
    Use pidfile to track running process.
    
    Signed-off-by: Seppo Takalo <seppo.takalo@nordicsemi.no>

--- app/scripts/sm_stop_ppp.sh
+++ app/scripts/sm_stop_ppp.sh
@@ -4,4 +4,19 @@
 #
 # SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
-test -f /var/run/ppp-nrf91.pid && kill -SIGTERM $(head -1 </var/run/ppp-nrf91.pid)
+PIDFILE="/var/run/nrf91-modem.pid"
+PPP_PIDFILE="/var/run/ppp-nrf91.pid"
+
+# Request PPPD to terminate
+if [ -f $PPP_PIDFILE ]; then
+        echo "Stopping PPP link..."
+        kill -SIGTERM $(head -1 <$PPP_PIDFILE)
+fi
+
+
+# Wait for Shutdown script to complete
+if [ -f $PIDFILE ]; then
+        echo "Waiting for Shutdown script to complete..."
+        timeout 12s tail --pid=$(head -1 <$PIDFILE) -f /dev/null \
+        || echo "Timeout waiting for Shutdown script to stop"
+fi
