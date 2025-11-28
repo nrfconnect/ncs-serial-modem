bb89bddb13fa3273ac074341329d35540959988d
Syver Haraldsen <syver.haraldsen@nordicsemi.no>
2025-11-28 15:31:45 +0100

    app: add support for MCUBoot, delta modem and full modem DFU
    This commit adds support for MCUBoot, delta modem and full modem DFU
    to the serial modem firmware.
    
    Signed-off-by: Syver Haraldsen <syver.haraldsen@nordicsemi.no>

--- app/src/sm_settings.c
+++ app/src/sm_settings.c
@@ -9,7 +9,9 @@
 #include <zephyr/kernel.h>
 #include <zephyr/settings/settings.h>
 #include <dfu/dfu_target.h>
+#include <errno.h>
 #include "sm_at_fota.h"
+#include "sm_at_dfu.h"
 #include "sm_settings.h"
 
 LOG_MODULE_REGISTER(sm_settings, CONFIG_SM_LOG_LEVEL);
@@ -20,6 +22,18 @@ static int settings_set(const char *name, size_t len, settings_read_cb read_cb, 
 		if (len != sizeof(sm_modem_full_fota))
 			return -EINVAL;
 		if (read_cb(cb_arg, &sm_modem_full_fota, len) > 0)
+			return 0;
+	}
+	if (!strcmp(name, "bootloader_mode_requested")) {
+		if (len != sizeof(sm_bootloader_mode_requested))
+			return -EINVAL;
+		if (read_cb(cb_arg, &sm_bootloader_mode_requested, len) > 0)
+			return 0;
+	}
+	if (!strcmp(name, "full_mfw_dfu_segment_type")) {
+		if (len != sizeof(full_mfw_dfu_segment_type))
+			return -EINVAL;
+		if (read_cb(cb_arg, &full_mfw_dfu_segment_type, len) > 0)
 			return 0;
 	}
 	/* Simply ignore obsolete settings that are not in use anymore.
@@ -59,4 +73,16 @@ int sm_settings_fota_save(void)
 {
 	return settings_save_one("sm/modem_full_fota",
 		&sm_modem_full_fota, sizeof(sm_modem_full_fota));
+}
+
+int sm_settings_bootloader_mode_save(void)
+{
+	return settings_save_one("sm/bootloader_mode_requested",
+		&sm_bootloader_mode_requested, sizeof(sm_bootloader_mode_requested));
+}
+
+int sm_settings_full_mfw_dfu_segment_type_save(void)
+{
+	return settings_save_one("sm/full_mfw_dfu_segment_type",
+		&full_mfw_dfu_segment_type, sizeof(full_mfw_dfu_segment_type));
 }
