b432e0ed1e333339d3cb2915eaf70ee69321ad5f
Juha Ylinen <juha.ylinen@nordicsemi.no>
2025-12-03 12:16:03 +0200

    tests: Add unit tests
    Add basic unit tests for socket AT commands.
    
    Jira: SM-133
    
    Signed-off-by: Juha Ylinen <juha.ylinen@nordicsemi.no>

---
+++ app/tests/stubs/control_pin_stubs.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+/**
+ * @file control_pin_stubs.c
+ * Stub implementations for control pin functions
+ */
+
+#include <zephyr/kernel.h>
+
+/* Stub for sm_ctrl_pin_enter_idle */
+void sm_ctrl_pin_enter_idle(void)
+{
+	/* Stub - no-op for tests */
+}
+
+/* Stub for sm_ctrl_pin_enter_sleep */
+void sm_ctrl_pin_enter_sleep(void)
+{
+	/* Stub - no-op for tests */
+}
