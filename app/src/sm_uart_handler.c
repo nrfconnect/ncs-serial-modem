8e9b35d4cbc44ed6bebc949f5aae4eff5a5b06bf
Markus Lassila <markus.lassila@nordicsemi.no>
2025-12-19 13:00:41 +0200

    app: Fix deadlock during modem crash dump collection
    When modem crash dump is collected over CMUX, the nrf_modem_lib_shutdown
    calls nrf_modem_lib_trace_processing_done_wait, which locks sm_work_q
    until traces have been sent.
    
    Pipe events need to be sent through system work queue.
    
    Signed-off-by: Markus Lassila <markus.lassila@nordicsemi.no>

--- app/src/sm_uart_handler.c
+++ app/src/sm_uart_handler.c
@@ -296,7 +296,10 @@ static inline void uart_callback_notify_pipe_transmit_idle(void)
 {
 #if SM_PIPE
 	if (atomic_test_bit(&sm_pipe.state, SM_PIPE_STATE_OPEN_BIT)) {
-		k_work_submit_to_queue(&sm_work_q, &sm_pipe.notify_transmit_idle);
+		/* This needs to be done in system work queue to avoid deadlock while
+		 * collecting modem crash dump.
+		 */
+		k_work_submit(&sm_pipe.notify_transmit_idle);
 	}
 #endif
 }
@@ -308,8 +311,10 @@ static inline void uart_callback_notify_pipe_closure(void)
 	    !atomic_test_bit(&sm_pipe.state, SM_PIPE_STATE_OPEN_BIT) &&
 	    !atomic_test_bit(&uart_state, SM_UART_STATE_RX_ENABLED_BIT) &&
 	    !atomic_test_bit(&uart_state, SM_UART_STATE_TX_ENABLED_BIT)) {
-		/* Pipe is closed, RX and TX are idle, notify the closure */
-		k_work_submit_to_queue(&sm_work_q, &sm_pipe.notify_closed);
+		/* Pipe is closed, RX and TX are idle, notify the closure.
+		 * This should be done in system work queue.
+		 */
+		k_work_submit(&sm_pipe.notify_closed);
 	}
 #endif
 }
