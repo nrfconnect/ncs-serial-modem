3664cd4dc74e75cce51c5481ba4a194549ff63ea
Seppo Takalo <seppo.takalo@nordicsemi.no>
2026-02-26 15:54:03 +0200

    app: Refine legacy CMUX and PPP behavior
    When AT#XCMUX and AT#XPPP are used, emulate the previous behavior
    with statically assigned channel numbers.
    
    Signed-off-by: Seppo Takalo <seppo.takalo@nordicsemi.no>

--- app/src/sm_cmux.h
+++ app/src/sm_cmux.h
@@ -6,20 +6,16 @@
 #ifndef SM_CMUX_
 #define SM_CMUX_
 
+#include <stdint.h>
 #include <stdbool.h>
 
 struct modem_pipe;
 
-/* Default CMUX channels. */
+/* Default CMUX channels when AT#XCMUX is used */
 enum cmux_channel {
 	CMUX_AT_CHANNEL = 1,
-#if defined(CONFIG_SM_PPP)
 	CMUX_PPP_CHANNEL = 2,
-#endif
 	CMUX_MODEM_TRACE_CHANNEL = 3,
-	CMUX_USER_CHANNEL_0,
-	CMUX_USER_CHANNEL_1,
-	CMUX_EXT_CHANNEL_COUNT
 };
 
 #if CONFIG_SM_CMUX
@@ -30,5 +26,14 @@ static inline bool sm_cmux_is_started(void)
 	return false;
 }
 #endif
+
+/**
+ * @brief Get a pointer to the modem_pipe associated with a given DLCI address.
+ *
+ * @param address The DLCI address.
+ * @return struct modem_pipe* Pointer to the modem_pipe,
+ *	   or NULL if there is no pipe associated with the given address.
+ */
+struct modem_pipe *sm_cmux_get_dlci(uint8_t address);
 
 #endif
