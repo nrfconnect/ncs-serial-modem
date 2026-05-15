db8dba6bdcc11b06eb19973607714bf07a32d081
divya pillai <divya.pillai@nordicsemi.no>
2026-05-15 16:08:40 +0200

    doc: CSS fixes revert
    CSS fixes revert
    
    Signed-off-by: divya pillai <divya.pillai@nordicsemi.no>

--- doc/conf.py
+++ doc/conf.py
@@ -10,7 +10,7 @@ import sys
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'ncs-serial-modem'
-copyright = '2025, Nordic Semiconductor'
+copyright = '2026, Nordic Semiconductor'
 author = 'Nordic Semiconductor'
 release = '0.1.0'
 
@@ -38,7 +38,9 @@ exclude_patterns = ['_build_sphinx', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_ncs_theme'
-html_theme_options = {'docsets': {},"addons_url": "","bare_metal_url": "","ncs_url": "https://docs.nordicsemi.com/bundle/ncs-latest/page/nrf/index.html", "ncs_label": "nRF Connect SDK docs", "logo_url": "https://docs.nordicsemi.com"}
+html_show_sphinx = False
+
+html_theme_options = {'docsets': {},"addons_url": "https://nrfconnect.github.io/ncs-app-index/","bare_metal_url": "","ncs_url": "https://docs.nordicsemi.com/bundle/ncs-latest/page/nrf/index.html", "ncs_label": "nRF Connect SDK Docs", "logo_url": "https://docs.nordicsemi.com"}
 
 html_extra_path = ['versions.json']
 
