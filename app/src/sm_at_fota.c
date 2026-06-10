nrfconnect/ncs-serial-modem

commit 506ede045d08b9939b2b1f0830339be7ba05eba5

App: Add MCUBoot update support
Add support for updating MCUBoot via both FOTA and DFU.

- Static partition map updated to include b0, s0 and s1 MCUBoot slots
- New AT#XBOOTINFO command to query the MCUboot firmware version and
  the active/inactive MCUBoot slot.
- AT#XFOTA extended with SM_FOTA_START_MCUBOOT_BL operation
- AT#XDFU commands extended with DFU_TYPE_MCUBOOT_BL type

Signed-off-by: Markus Lassila <markus.lassila@nordicsemi.no>

app/src/sm_at_fota.c
@@ -12,10 +12,11 @@
 #include <zephyr/logging/log_ctrl.h>
 #include <zephyr/net/tls_credentials.h>
 #include <zephyr/net/http/parser_url.h>
+#include <strings.h>
 #include <zephyr/device.h>
 #include <zephyr/sys/reboot.h>
 #include <net/fota_download.h>
-#include <fota_download_util.h>
+#include <pm_config.h>
 #include <dfu/dfu_target_full_modem.h>
 #include <dfu/fmfu_fdev.h>
 #include "sm_util.h"
@@ -26,12 +27,6 @@
 
 LOG_MODULE_REGISTER(sm_fota, CONFIG_SM_LOG_LEVEL);
 
-/* file_uri: scheme://hostname[:port]path[?parameters] */
-#define FILE_URI_MAX	CONFIG_DOWNLOADER_MAX_FILENAME_SIZE
-#define SCHEMA_HTTP     "http"
-#define SCHEMA_HTTPS	"https"
-#define URI_HOST_MAX	CONFIG_DOWNLOADER_MAX_HOSTNAME_SIZE
-#define URI_SCHEMA_MAX	8
 #define ERASE_POLL_TIME 2
 
 /* Some features need fota_download update */
@@ -43,19 +38,20 @@ enum sm_fota_operation {
 	SM_FOTA_START_MFW = 2,
 	SM_FOTA_START_FULL_FOTA = 3,
 	SM_FOTA_PAUSE_RESUME = 4,
+	SM_FOTA_START_MCUBOOT_BL = 5,
 	SM_FOTA_MFW_READ = 7,
 	SM_FOTA_ERASE_MFW = 9
 };
 
 bool sm_modem_full_fota;
+bool sm_fota_bl_pending_validate;
+uint32_t sm_fota_bl_version_before;
 
-uint8_t sm_fota_type = DFU_TARGET_IMAGE_TYPE_NONE;
+enum sm_fota_image_type sm_fota_type = SM_FOTA_TYPE_NONE;
 enum fota_stage sm_fota_stage = FOTA_STAGE_INIT;
 enum fota_status sm_fota_status;
 int32_t sm_fota_info;
 
-static char path[FILE_URI_MAX];
-static char hostname[URI_HOST_MAX];
 static struct modem_pipe *fota_pipe;
 
 #if defined(CONFIG_SM_FULL_FOTA)
@@ -190,93 +186,76 @@ static int do_fota_erase_mfw(void)
 	return 0;
 }
 
-static int do_fota_start(int op, const char *file_uri, int sec_tag,
+static int do_fota_start(const char *file_uri, size_t file_uri_len, int sec_tag,
 			 uint8_t pdn_id, enum dfu_target_image_type type)
 {
 	int ret;
 	struct http_parser_url parser = {
-		/* UNINIT checker fix, assumed UF_SCHEMA existence */
 		.field_set = 0
 	};
-	char schema[URI_SCHEMA_MAX];
+	size_t path_off;
+	char *hostname = NULL;
+	char *path = NULL;
 
 	http_parser_url_init(&parser);
-	ret = http_parser_parse_url(file_uri, strlen(file_uri), 0, &parser);
+	ret = http_parser_parse_url(file_uri, file_uri_len, 0, &parser);
 	if (ret) {
 		LOG_ERR("Parse URL error");
 		return -EINVAL;
 	}
 
-	/* Schema stores http/https information */
-	memset(schema, 0x00, 8);
-	if (parser.field_set & (1 << UF_SCHEMA)) {
-		if (parser.field_data[UF_SCHEMA].len < URI_SCHEMA_MAX) {
-			strncpy(schema, file_uri + parser.field_data[UF_SCHEMA].off,
-				parser.field_data[UF_SCHEMA].len);
-		} else {
-			LOG_ERR("URL schema length %d too long, exceeds the max length of %d",
-				parser.field_data[UF_SCHEMA].len, URI_SCHEMA_MAX);
-			return -ENOMEM;
-		}
-	} else {
-		LOG_ERR("Parse schema error");
-		return -EINVAL;
-	}
-
-	/* Path includes folder and file information */
-	/* This stores also the query data after folder and file description */
-	memset(path, 0x00, FILE_URI_MAX);
-	if (parser.field_set & (1 << UF_PATH)) {
-		/* Remove the leading '/' as some HTTP servers don't like it */
-		if ((strlen(file_uri) - parser.field_data[UF_PATH].off + 1) < FILE_URI_MAX) {
-			strncpy(path, file_uri + parser.field_data[UF_PATH].off + 1,
-				strlen(file_uri) - parser.field_data[UF_PATH].off - 1);
-		} else {
-			LOG_ERR("URL path length %d too long, exceeds the max length of %d",
-					strlen(file_uri) - parser.field_data[UF_PATH].off - 1,
-					FILE_URI_MAX);
-			return -ENOMEM;
-		}
-	} else {
+	if (!(parser.field_set & (1 << UF_PATH))) {
 		LOG_ERR("Parse path error");
 		return -EINVAL;
 	}
 
-	/* Stores everything before path (schema, hostname, port) */
-	memset(hostname, 0x00, URI_HOST_MAX);
-	if (parser.field_set & (1 << UF_HOST)) {
-		if (parser.field_data[UF_PATH].off < URI_HOST_MAX) {
-			strncpy(hostname, file_uri, parser.field_data[UF_PATH].off);
-		} else {
-			LOG_ERR("URL host name length %d too long, exceeds the max length of %d",
-					parser.field_data[UF_PATH].off, URI_HOST_MAX);
-			return -ENOMEM;
-		}
-	} else {
+	if (!(parser.field_set & (1 << UF_HOST))) {
 		LOG_ERR("Parse host error");
 		return -EINVAL;
 	}
 
-	/* start HTTP(S) FOTA */
-	if (sm_util_casecmp(schema, SCHEMA_HTTPS)) {
+	path_off = parser.field_data[UF_PATH].off;
+
+	/* host: scheme + authority (everything before the path), e.g. "https://host:port" */
+	hostname = k_malloc(path_off + 1);
+	if (!hostname) {
+		return -ENOMEM;
+	}
+	memcpy(hostname, file_uri, path_off);
+	hostname[path_off] = '\0';
+
+	/* path: URI content after the leading '/' */
+	path = k_malloc(file_uri_len - path_off);
+	if (!path) {
+		k_free(hostname);
+		return -ENOMEM;
+	}
+	memcpy(path, file_uri + path_off + 1, file_uri_len - path_off - 1);
+	path[file_uri_len - path_off - 1] = '\0';
+
+	/* start HTTP(S) FOTA (hostname buffer includes scheme and port, see http_parser) */
+	if (strncasecmp(hostname, "https://", strlen("https://")) == 0) {
 		if (sec_tag == SEC_TAG_TLS_INVALID) {
 			LOG_ERR("Missing sec_tag");
-			return -EINVAL;
+			ret = -EINVAL;
+		} else {
+			ret = fota_download_start_with_image_type(hostname, path, sec_tag, pdn_id,
+								  0, type);
 		}
-		ret = fota_download_start_with_image_type(hostname, path, sec_tag, pdn_id, 0, type);
-	} else if (sm_util_casecmp(schema, SCHEMA_HTTP)) {
+	} else if (strncasecmp(hostname, "http://", strlen("http://")) == 0) {
 		ret = fota_download_start_with_image_type(hostname, path, -1, pdn_id, 0, type);
 	} else {
 		ret = -EINVAL;
 	}
+
+	k_free(hostname);
+	k_free(path);
+
 	/* Send an URC if failed to start */
 	if (ret) {
 		rsp_send("\r\n#XFOTA: %d,%d,%d\r\n", FOTA_STAGE_DOWNLOAD,
 			FOTA_STATUS_ERROR, ret);
 	}
-
-	sm_fota_init_state();
-	sm_fota_type = type;
 
 	return ret;
 }
@@ -294,7 +273,10 @@ static void fota_dl_handler(const struct fota_download_evt *evt)
 	case FOTA_DOWNLOAD_EVT_FINISHED:
 		sm_fota_stage = FOTA_STAGE_ACTIVATE;
 		sm_fota_info = 0;
-		sm_modem_full_fota = (sm_fota_type == DFU_TARGET_IMAGE_TYPE_FULL_MODEM);
+		sm_modem_full_fota = (sm_fota_type == SM_FOTA_TYPE_FULL_MFW);
+#if defined(PM_S1_ADDRESS)
+		sm_fota_bl_pending_validate = (sm_fota_type == SM_FOTA_TYPE_MCUBOOT_BL);
+#endif
 		/* Save, in case reboot by reset */
 		sm_settings_fota_save();
 		urc_send_to(fota_pipe, "\r\n#XFOTA: %d,%d\r\n", sm_fota_stage, sm_fota_status);
@@ -358,30 +340,43 @@ static int handle_at_fota(enum at_parser_cmd_type cmd_type, struct at_parser *pa
 			break;
 		case SM_FOTA_START_APP:
 		case SM_FOTA_START_MFW:
+#if defined(PM_S1_ADDRESS)
+		case SM_FOTA_START_MCUBOOT_BL:
+#endif
 #if defined(CONFIG_SM_FULL_FOTA)
 		case SM_FOTA_START_FULL_FOTA:
 #endif
-			char uri[FILE_URI_MAX];
-			uint16_t pdn_id;
-			int size = FILE_URI_MAX;
+		{
+			/* We cannot handle multiple simultaneous FOTA activations. */
+			if (sm_fota_stage == FOTA_STAGE_ACTIVATE) {
+				err = -EBUSY;
+				break;
+			}
+
+			uint16_t pdn_id = 0;
 			sec_tag_t sec_tag = SEC_TAG_TLS_INVALID;
 			enum dfu_target_image_type type;
+			const char *uri_ptr;
+			size_t uri_len;
 
-			err = util_string_get(parser, 2, uri, &size);
+			err = at_parser_string_ptr_get(parser, 2, &uri_ptr, &uri_len);
 			if (err) {
-				return err;
+				break;
 			}
 			if (param_count > 3) {
 				at_parser_num_get(parser, 3, &sec_tag);
 			}
-			if (op == SM_FOTA_START_APP) {
+			if (param_count > 4) {
+				at_parser_num_get(parser, 4, &pdn_id);
+			}
+			if (op == SM_FOTA_START_APP || op == SM_FOTA_START_MCUBOOT_BL) {
 				type = DFU_TARGET_IMAGE_TYPE_MCUBOOT;
 			}
 #if defined(CONFIG_SM_FULL_FOTA)
 			else if (op == SM_FOTA_START_FULL_FOTA) {
 				err = setup_full_modem_fota_config();
 				if (err != 0) {
-					return err;
+					break;
 				}
 				type = DFU_TARGET_IMAGE_TYPE_FULL_MODEM;
 			}
@@ -389,13 +384,30 @@ static int handle_at_fota(enum at_parser_cmd_type cmd_type, struct at_parser *pa
 			else {
 				type = DFU_TARGET_IMAGE_TYPE_MODEM_DELTA;
 			}
-			if (param_count > 4) {
-				at_parser_num_get(parser, 4, &pdn_id);
-				err = do_fota_start(op, uri, sec_tag, pdn_id, type);
-			} else {
-				err = do_fota_start(op, uri, sec_tag, 0, type);
+			err = do_fota_start(uri_ptr, uri_len, sec_tag, pdn_id, type);
+			sm_fota_init_state();
+			if (op == SM_FOTA_START_MFW) {
+				sm_fota_type = SM_FOTA_TYPE_MFW;
+			}
+#if defined(CONFIG_SM_FULL_FOTA)
+			else if (op == SM_FOTA_START_FULL_FOTA) {
+				sm_fota_type = SM_FOTA_TYPE_FULL_MFW;
+			}
+#endif
+#if defined(PM_S1_ADDRESS)
+			else if (op == SM_FOTA_START_MCUBOOT_BL) {
+				sm_fota_type = SM_FOTA_TYPE_MCUBOOT_BL;
+
+				/* Save the MCUboot version before FOTA. */
+				sm_util_mcuboot_active_version(&sm_fota_bl_version_before);
+				sm_settings_fota_save();
+			}
+#endif
+			else {
+				sm_fota_type = SM_FOTA_TYPE_APP;
 			}
 			break;
+		}
 #if FOTA_FUTURE_FEATURE
 		case SM_FOTA_PAUSE_RESUME:
 			if (paused) {
@@ -421,10 +433,20 @@ static int handle_at_fota(enum at_parser_cmd_type cmd_type, struct at_parser *pa
 		break;
 
 	case AT_PARSER_CMD_TYPE_TEST:
-#if defined(CONFIG_SM_FULL_FOTA)
+#if defined(PM_S1_ADDRESS) && defined(CONFIG_SM_FULL_FOTA)
+		rsp_send("\r\n#XFOTA: "
+			 "(%d,%d,%d,%d,%d,%d,%d)[,<file_url>[,<sec_tag>[,<pdn_id>]]]\r\n",
+			 SM_FOTA_STOP, SM_FOTA_START_APP, SM_FOTA_START_MFW,
+			 SM_FOTA_START_FULL_FOTA, SM_FOTA_START_MCUBOOT_BL, SM_FOTA_MFW_READ,
+			 SM_FOTA_ERASE_MFW);
+#elif defined(PM_S1_ADDRESS)
 		rsp_send("\r\n#XFOTA: (%d,%d,%d,%d,%d,%d)[,<file_url>[,<sec_tag>[,<pdn_id>]]]\r\n",
-			SM_FOTA_STOP, SM_FOTA_START_APP, SM_FOTA_START_MFW,
-			SM_FOTA_MFW_READ, SM_FOTA_ERASE_MFW, SM_FOTA_START_FULL_FOTA);
+			 SM_FOTA_STOP, SM_FOTA_START_APP, SM_FOTA_START_MFW,
+			 SM_FOTA_START_MCUBOOT_BL, SM_FOTA_MFW_READ, SM_FOTA_ERASE_MFW);
+#elif defined(CONFIG_SM_FULL_FOTA)
+		rsp_send("\r\n#XFOTA: (%d,%d,%d,%d,%d,%d)[,<file_url>[,<sec_tag>[,<pdn_id>]]]\r\n",
+			 SM_FOTA_STOP, SM_FOTA_START_APP, SM_FOTA_START_MFW,
+			 SM_FOTA_START_FULL_FOTA, SM_FOTA_MFW_READ, SM_FOTA_ERASE_MFW);
 #else
 		rsp_send("\r\n#XFOTA: (%d,%d,%d,%d,%d)[,<file_url>[,<sec_tag>[,<pdn_id>]]]\r\n",
 			SM_FOTA_STOP, SM_FOTA_START_APP, SM_FOTA_START_MFW,
@@ -449,6 +471,7 @@ static int sm_at_fota_init(void)
 		sm_init_failed = true;
 		return ret;
 	}
+
 	return 0;
 }
 SYS_INIT(sm_at_fota_init, APPLICATION, 0);
@@ -456,10 +479,40 @@ SYS_INIT(sm_at_fota_init, APPLICATION, 0);
 void sm_fota_init_state(void)
 {
 	sm_modem_full_fota = false;
-	sm_fota_type = DFU_TARGET_IMAGE_TYPE_NONE;
+	sm_fota_type = SM_FOTA_TYPE_NONE;
 	sm_fota_stage = FOTA_STAGE_INIT;
 	sm_fota_status = FOTA_STATUS_OK;
 	sm_fota_info = 0;
+}
+
+void sm_fota_mcuboot_bl_boot_check(void)
+{
+#if defined(PM_S1_ADDRESS)
+	if (!sm_fota_bl_pending_validate) {
+		return;
+	}
+
+	uint32_t current_version = 0;
+	int err = sm_util_mcuboot_active_version(&current_version);
+
+	if (err != 0) {
+		sm_fota_status = FOTA_STATUS_ERROR;
+		sm_fota_info = err;
+		LOG_ERR("MCUboot validate: FAIL (fw_info read err %d)", err);
+	} else if (current_version <= sm_fota_bl_version_before) {
+		sm_fota_status = FOTA_STATUS_ERROR;
+		sm_fota_info = 1;
+		LOG_WRN("MCUboot validate: FAIL (version unchanged: %u -> %u)",
+			sm_fota_bl_version_before, current_version);
+	} else {
+		sm_fota_status = FOTA_STATUS_OK;
+		sm_fota_info = 0;
+	}
+
+	sm_fota_type = SM_FOTA_TYPE_MCUBOOT_BL;
+	sm_fota_stage = FOTA_STAGE_COMPLETE;
+	sm_fota_bl_pending_validate = false;
+#endif
 }
 
 #if defined(CONFIG_LWM2M_CARRIER)
@@ -482,7 +535,7 @@ bool lwm2m_os_dfu_application_update_validate(void)
 void sm_fota_post_process(void)
 {
 #if defined(CONFIG_LWM2M_CARRIER)
-	if ((sm_fota_type == DFU_TARGET_IMAGE_TYPE_MCUBOOT) &&
+	if ((sm_fota_type == SM_FOTA_TYPE_APP || sm_fota_type == SM_FOTA_TYPE_MCUBOOT_BL) &&
 	    (sm_fota_status == FOTA_STATUS_OK) &&
 	    (sm_fota_stage == FOTA_STAGE_COMPLETE)) {
 		carrier_app_fota_success = true;

app/pm_static_nrf9151dk_nrf9151_ns.yml
@@ -1,49 +1,102 @@
-app:
-  address: 0x18000
-  end_address: 0x86000
-  placement:
-    align:
-      start: 0x8000
-  region: flash_primary
-  size: 0x6e000
-mcuboot:
+b0:
   address: 0x0
-  end_address: 0x10000
+  end_address: 0x8000
   region: flash_primary
-  size: 0x10000
+  size: 0x8000
+s0_pad:
+  address: 0x8000
+  end_address: 0x8200
+  region: flash_primary
+  size: 0x200
+  share_size:
+  - mcuboot_pad
+s0_image:
+  address: 0x8200
+  end_address: 0x14000
+  region: flash_primary
+  size: 0xbe00
+  span:
+  - mcuboot
+s0:
+  address: 0x8000
+  end_address: 0x14000
+  region: flash_primary
+  size: 0xc000
+  span:
+  - mcuboot
+  - s0_pad
+mcuboot:
+  address: 0x8200
+  end_address: 0x14000
+  region: flash_primary
+  size: 0xbe00
+  sharers: 0x1
+s1_pad:
+  address: 0x14000
+  end_address: 0x14200
+  region: flash_primary
+  size: 0x200
+  share_size:
+  - mcuboot_pad
+s1_image:
+  address: 0x14200
+  end_address: 0x20000
+  region: flash_primary
+  size: 0xbe00
+  share_size:
+  - mcuboot
+s1:
+  address: 0x14000
+  end_address: 0x20000
+  region: flash_primary
+  size: 0xc000
+  span:
+  - s1_pad
+  - s1_image
 mcuboot_pad:
-  address: 0x10000
-  end_address: 0x10200
+  address: 0x20000
+  end_address: 0x20200
   placement:
     align:
       start: 0x8000
   region: flash_primary
   size: 0x200
+  sharers: 0x2
 mcuboot_primary:
-  address: 0x10000
-  end_address: 0x86000
+  address: 0x20000
+  end_address: 0x8f000
   region: flash_primary
   sharers: 0x1
-  size: 0x76000
+  size: 0x6f000
   span:
   - tfm
   - app
   - mcuboot_pad
-mcuboot_primary_app:
-  address: 0x10200
-  end_address: 0x86000
+  - mcuboot_primary_swap
+  - mcuboot_primary_trailer
+mcuboot_primary_swap:
+  address: 0x8d000
+  end_address: 0x8e000
   region: flash_primary
-  size: 0x75e00
+  size: 0x1000
+mcuboot_primary_trailer:
+  address: 0x8e000
+  end_address: 0x8f000
+  region: flash_primary
+  size: 0x1000
+mcuboot_primary_app:
+  address: 0x20200
+  end_address: 0x8d000
+  region: flash_primary
+  size: 0x6ce00
   span:
   - app
   - tfm
 mcuboot_secondary:
-  address: 0x86000
-  end_address: 0xfc000
+  address: 0x8f000
+  end_address: 0xfd000
   region: flash_primary
-  share_size:
-  - mcuboot_primary
-  size: 0x76000
+  size: 0x6e000
 mcuboot_sram:
   address: 0x20000000
   end_address: 0x20006000
@@ -52,24 +105,29 @@ mcuboot_sram:
   span:
   - tfm_sram
 nonsecure_storage:
-  address: 0xfc000
+  address: 0xfd000
   end_address: 0x100000
   region: flash_primary
-  size: 0x4000
+  size: 0x3000
   span:
   - settings_storage
 otp:
-  address: 0xff8108
+  address: 0xff8388
   end_address: 0xff83fc
   region: otp
-  size: 0x2f4
+  size: 0x74
+provision:
+  address: 0xff8108
+  end_address: 0xff8388
+  region: otp
+  size: 0x280
 settings_storage:
-  address: 0xfc000
+  address: 0xfd000
   end_address: 0x100000
   inside:
   - nonsecure_storage
   region: flash_primary
-  size: 0x4000
+  size: 0x3000
 sram_secure:
   address: 0x20000000
   end_address: 0x20006000
@@ -77,26 +135,34 @@ sram_secure:
   size: 0x6000
   span:
   - tfm_sram
+app:
+  address: 0x28000
+  end_address: 0x8d000
+  placement:
+    align:
+      start: 0x8000
+  region: flash_primary
+  size: 0x65000
 tfm:
-  address: 0x10200
-  end_address: 0x18000
+  address: 0x20200
+  end_address: 0x28000
   inside:
   - mcuboot_primary_app
   region: flash_primary
   size: 0x7e00
 tfm_nonsecure:
-  address: 0x18000
-  end_address: 0x86000
+  address: 0x28000
+  end_address: 0x8d000
   placement:
     align:
       start: 0x8000
   region: flash_primary
-  size: 0x6e000
+  size: 0x65000
   span:
   - app
 tfm_secure:
-  address: 0x10000
-  end_address: 0x18000
+  address: 0x20000
+  end_address: 0x28000
   region: flash_primary
   size: 0x8000
   span:

app/pm_static_nrf9151dk_nrf9151_ns_full_fota.yml
@@ -2,52 +2,95 @@
 # CONFIG_TFM_PROFILE_TYPE_NOT_SET.
 # TODO: PPP+CMUX doesn't fit with this configuration at the moment.
 #       Partition table has to be optimized later.
-app:
-  address: 0x38000
-  end_address: 0x7e000
-  placement:
-    align:
-      start: 0x8000
-  region: flash_primary
-  size: 0x46000
-mcuboot:
+b0:
   address: 0x0
-  end_address: 0x10000
+  end_address: 0x8000
   region: flash_primary
-  size: 0x10000
+  size: 0x8000
+s0_pad:
+  address: 0x8000
+  end_address: 0x8200
+  region: flash_primary
+  size: 0x200
+  share_size:
+  - mcuboot_pad
+s0_image:
+  address: 0x8200
+  end_address: 0x14000
+  region: flash_primary
+  size: 0xbe00
+  span:
+  - mcuboot
+s0:
+  address: 0x8000
+  end_address: 0x14000
+  region: flash_primary
+  size: 0xc000
+  span:
+  - mcuboot
+  - s0_pad
+mcuboot:
+  address: 0x8200
+  end_address: 0x14000
+  region: flash_primary
+  size: 0xbe00
+  sharers: 0x1
+s1_pad:
+  address: 0x14000
+  end_address: 0x14200
+  region: flash_primary
+  size: 0x200
+  share_size:
+  - mcuboot_pad
+s1_image:
+  address: 0x14200
+  end_address: 0x20000
+  region: flash_primary
+  size: 0xbe00
+  share_size:
+  - mcuboot
+s1:
+  address: 0x14000
+  end_address: 0x20000
+  region: flash_primary
+  size: 0xc000
+  span:
+  - s1_pad
+  - s1_image
 mcuboot_pad:
-  address: 0x10000
-  end_address: 0x10200
+  address: 0x20000
+  end_address: 0x20200
   placement:
     align:
       start: 0x8000
   region: flash_primary
   size: 0x200
+  sharers: 0x2
 mcuboot_primary:
-  address: 0x10000
-  end_address: 0x86000
+  address: 0x20000
+  end_address: 0x8A000
   region: flash_primary
   sharers: 0x1
-  size: 0x76000
+  size: 0x6a000
   span:
   - tfm
   - app
   - mcuboot_pad
 mcuboot_primary_app:
-  address: 0x10200
-  end_address: 0x86000
+  address: 0x20200
+  end_address: 0x8A000
   region: flash_primary
-  size: 0x75e00
+  size: 0x89E00
   span:
   - app
   - tfm
 mcuboot_secondary:
-  address: 0x86000
-  end_address: 0xfc000
+  address: 0x8A000
+  end_address: 0xf4000
   region: flash_primary
   share_size:
   - mcuboot_primary
-  size: 0x76000
+  size: 0x6A000
 mcuboot_sram:
   address: 0x20000000
   end_address: 0x20006000
@@ -56,20 +99,25 @@ mcuboot_sram:
   span:
   - tfm_sram
 nonsecure_storage:
-  address: 0xfc000
-  end_address: 0x100000
+  address: 0xf4000
+  end_address: 0xf8000
   region: flash_primary
   size: 0x4000
   span:
   - settings_storage
 otp:
-  address: 0xff8108
+  address: 0xff8388
   end_address: 0xff83fc
   region: otp
-  size: 0x2f4
+  size: 0x74
+provision:
+  address: 0xff8108
+  end_address: 0xff8388
+  region: otp
+  size: 0x280
 settings_storage:
-  address: 0xfc000
-  end_address: 0x100000
+  address: 0xf4000
+  end_address: 0xf8000
   inside:
   - nonsecure_storage
   region: flash_primary
@@ -81,47 +129,55 @@ sram_secure:
   size: 0x16000
   span:
   - tfm_sram
+app:
+  address: 0x48000
+  end_address: 0x8A000
+  placement:
+    align:
+      start: 0x8000
+  region: flash_primary
+  size: 0x42000
 tfm:
-  address: 0x10200
-  end_address: 0x38000
+  address: 0x20200
+  end_address: 0x48000
   inside:
   - mcuboot_primary_app
   region: flash_primary
   size: 0x27e00
 tfm_its:
-  address: 0x7e000
-  end_address: 0x80000
+  address: 0xF8000
+  end_address: 0xFA000
   inside:
   - tfm_storage
   region: flash_primary
   size: 0x2000
 tfm_nonsecure:
-  address: 0x38000
-  end_address: 0x7e000
+  address: 0x48000
+  end_address: 0x8A000
   placement:
     align:
       start: 0x8000
   region: flash_primary
-  size: 0x46000
+  size: 0x42000
   span:
   - app
 tfm_otp_nv_counters:
-  address: 0x80000
-  end_address: 0x82000
+  address: 0xFA000
+  end_address: 0xFC000
   inside:
   - tfm_storage
   region: flash_primary
   size: 0x2000
 tfm_ps:
-  address: 0x82000
-  end_address: 0x86000
+  address: 0xFC000
+  end_address: 0x100000
   inside:
   - tfm_storage
   region: flash_primary
   size: 0x4000
 tfm_secure:
-  address: 0x10200
-  end_address: 0x38000
+  address: 0x20200
+  end_address: 0x48000
   region: flash_primary
   size: 0x27e00
   span:
@@ -134,8 +190,8 @@ tfm_sram:
   region: sram_primary
   size: 0x16000
 tfm_storage:
-  address: 0x7e000
-  end_address: 0x86000
+  address: 0xF8000
+  end_address: 0x100000
   region: flash_primary
   size: 0x8000
   span:

app/prj.conf
@@ -77,8 +77,9 @@ CONFIG_DFU_TARGET=y
 CONFIG_DOWNLOADER=y
 CONFIG_DOWNLOADER_STACK_SIZE=4096
 CONFIG_DOWNLOADER_MAX_FILENAME_SIZE=2048
-CONFIG_BOOTLOADER_MCUBOOT=y
 CONFIG_IMG_MANAGER=y
+# MCUboot bootloader FOTA requires fw_info to validate the update after reboot.
+CONFIG_FW_INFO=y
 
 # FLASH
 CONFIG_FLASH=y

app/scripts/sm_dfu_host.py
@@ -204,11 +204,18 @@ def parse_chunks_cbor(filepath: str, chunk_size: int = 4096) -> tuple[list, list
 DFU_TYPE_APP = 0
 DFU_TYPE_DELTA = 1
 DFU_TYPE_FULL = 2
+DFU_TYPE_MCUBOOT_BL = 3
 
 
 def dfu_init(dfu_type: int, size: int = None) -> bool:
-    """Initialize DFU. Size required for APP/DELTA. FULL reboots device."""
+    """Initialize DFU. Size required for APP/DELTA/MCUBOOT_BL. FULL reboots device."""
     if dfu_type == DFU_TYPE_APP:
+        if size is None:
+            return False
+        print(f"AT#XDFUINIT={dfu_type},{size}")
+        send_command("AT#XDFUINIT", dfu_type, size)
+        expected, timeout = "OK", 5.0
+    elif dfu_type == DFU_TYPE_MCUBOOT_BL:
         if size is None:
             return False
         print(f"AT#XDFUINIT={dfu_type},{size}")
@@ -305,6 +312,53 @@ def do_dfu_app(filepath: str, retries: int = 3) -> bool:
         return False
 
     print("OK: Application DFU complete. Reboot to activate.")
+    return True
+
+
+def do_dfu_mcuboot_bootloader(filepath: str, retries: int = 3) -> bool:
+    """Perform MCUboot second-stage bootloader DFU.
+
+    The image must be the signed candidate for the *inactive* s0/s1 bank.
+    """
+    chunks = parse_chunks_bin(filepath)
+    total_size = sum(len(data) for _, data in chunks)
+    total_chunks = len(chunks)
+
+    print(
+        "MCUboot bootloader DFU: ensure this .bin is the inactive-slot signed image "
+        "(s0 vs s1 package from your NCS build output)."
+    )
+    print(f"MCUboot bootloader DFU: {total_size:,} bytes in {total_chunks} chunks")
+
+    if not dfu_init(DFU_TYPE_MCUBOOT_BL, total_size):
+        print("\nERROR: Init failed (is NSIB + MCUboot enabled?)")
+        return False
+
+    bytes_sent = 0
+    start_time = time.time()
+
+    for i, (addr, data) in enumerate(chunks, 1):
+        success = False
+        urc = ""
+        for _ in range(retries):
+            success, urc = dfu_write(DFU_TYPE_MCUBOOT_BL, addr, data)
+            if success:
+                break
+        if not success:
+            print(f"\nERROR: Chunk {i} failed after {retries} retries")
+            return False
+
+        bytes_sent += len(data)
+        cmd = f"AT#XDFUWRITE={DFU_TYPE_MCUBOOT_BL},{addr},{len(data)} -> {urc}"
+        print_progress("MCUboot bootloader", i, total_chunks, bytes_sent, total_size, start_time, cmd)
+
+    print()
+
+    if not dfu_apply(DFU_TYPE_MCUBOOT_BL):
+        print("ERROR: Apply failed")
+        return False
+
+    print("OK: MCUboot bootloader DFU apply complete. Reboot to activate the new MCUboot. Validate the version increase with AT#XBOOTINFO=0.")
     return True
 
 
@@ -458,8 +512,11 @@ def main() -> int:
     parser.add_argument("--port", required=True, help="Serial port")
     parser.add_argument("--baudrate", required=True, type=int, help="Baud rate")
     parser.add_argument("--file", help="Firmware file path")
-    parser.add_argument("--type", choices=["application", "modem-delta", "modem-full"],
-                        help="DFU type")
+    parser.add_argument(
+        "--type",
+        choices=["application", "modem-delta", "modem-full", "mcuboot-bootloader"],
+        help="DFU type",
+    )
     parser.add_argument("--ping", action="store_true", help="Ping device (AT)")
     parser.add_argument("--reset", action="store_true", help="Reset device (AT#XRESET)")
     parser.add_argument("--modem-reset", action="store_true",
@@ -491,7 +548,7 @@ def main() -> int:
         elif args.reset:
             do_reset()
             success = True
-        elif getattr(args, 'modem_reset', False):
+        elif getattr(args, "modem_reset", False):
             do_modem_reset()
             success = True
         else:
@@ -504,6 +561,8 @@ def main() -> int:
 
             if args.type == "application":
                 success = do_dfu_app(str(filepath))
+            elif args.type == "mcuboot-bootloader":
+                success = do_dfu_mcuboot_bootloader(str(filepath))
             elif args.type == "modem-delta":
                 success = do_dfu_delta(str(filepath))
             elif args.type == "modem-full":

commit 4dc2045b0ea47d68ec11ed39ed4e20601d6b5072

app: Fix pause-resume with FOTA
Retain the memory for hostname and path until the FOTA downloader
is ready.

This was regression from 506ede045d08b9939b2b1f0830339be7ba05eba5.

Signed-off-by: Markus Lassila <markus.lassila@nordicsemi.no>

app/src/sm_at_fota.c
@@ -59,6 +59,13 @@ int32_t sm_fota_info;
 
 static struct modem_pipe *fota_pipe;
 
+/* Heap buffers for the current FOTA hostname and path.
+ * fota_download stores raw pointers to these, so they must
+ * remain valid until the download is complete.
+ */
+static char *fota_hostname;
+static char *fota_path;
+
 #if defined(CONFIG_SM_FULL_FOTA)
 /* Buffer used as temporary storage when downloading the modem firmware.
  */
@@ -199,8 +206,12 @@ static int do_fota_start(const char *file_uri, size_t file_uri_len, int sec_tag,
 		.field_set = 0
 	};
 	size_t path_off;
-	char *hostname = NULL;
-	char *path = NULL;
+
+	/* Safety free in case a terminal event was somehow missed. */
+	k_free(fota_hostname);
+	fota_hostname = NULL;
+	k_free(fota_path);
+	fota_path = NULL;
 
 	http_parser_url_init(&parser);
 	ret = http_parser_parse_url(file_uri, file_uri_len, 0, &parser);
@@ -222,39 +233,46 @@ static int do_fota_start(const char *file_uri, size_t file_uri_len, int sec_tag,
 	path_off = parser.field_data[UF_PATH].off;
 
 	/* host: scheme + authority (everything before the path), e.g. "https://host:port" */
-	hostname = k_malloc(path_off + 1);
-	if (!hostname) {
+	fota_hostname = k_malloc(path_off + 1);
+	if (!fota_hostname) {
 		return -ENOMEM;
 	}
-	memcpy(hostname, file_uri, path_off);
-	hostname[path_off] = '\0';
+	memcpy(fota_hostname, file_uri, path_off);
+	fota_hostname[path_off] = '\0';
 
 	/* path: URI content after the leading '/' */
-	path = k_malloc(file_uri_len - path_off);
-	if (!path) {
-		k_free(hostname);
+	fota_path = k_malloc(file_uri_len - path_off);
+	if (!fota_path) {
+		k_free(fota_hostname);
+		fota_hostname = NULL;
 		return -ENOMEM;
 	}
-	memcpy(path, file_uri + path_off + 1, file_uri_len - path_off - 1);
-	path[file_uri_len - path_off - 1] = '\0';
+	memcpy(fota_path, file_uri + path_off + 1, file_uri_len - path_off - 1);
+	fota_path[file_uri_len - path_off - 1] = '\0';
 
-	/* start HTTP(S) FOTA (hostname buffer includes scheme and port, see http_parser) */
-	if (strncasecmp(hostname, "https://", strlen("https://")) == 0) {
+	/* start HTTP(S) FOTA (fota_hostname includes scheme and port, see http_parser) */
+	if (strncasecmp(fota_hostname, "https://", strlen("https://")) == 0) {
 		if (sec_tag == SEC_TAG_TLS_INVALID) {
 			LOG_ERR("Missing sec_tag");
 			ret = -EINVAL;
 		} else {
-			ret = fota_download_start_with_image_type(hostname, path, sec_tag, pdn_id,
-								  0, type);
+			ret = fota_download_start_with_image_type(fota_hostname, fota_path,
+								  sec_tag, pdn_id, 0, type);
 		}
-	} else if (strncasecmp(hostname, "http://", strlen("http://")) == 0) {
-		ret = fota_download_start_with_image_type(hostname, path, -1, pdn_id, 0, type);
+	} else if (strncasecmp(fota_hostname, "http://", strlen("http://")) == 0) {
+		ret = fota_download_start_with_image_type(fota_hostname, fota_path,
+							  -1, pdn_id, 0, type);
 	} else {
 		ret = -EINVAL;
 	}
 
-	k_free(hostname);
-	k_free(path);
+	if (ret) {
+		/* Failed to start; fota_download won't fire a terminal event, so free now. */
+		k_free(fota_hostname);
+		fota_hostname = NULL;
+		k_free(fota_path);
+		fota_path = NULL;
+	}
 
 	/* Send an URC if failed to start */
 	if (ret) {
@@ -276,6 +294,11 @@ static void fota_dl_handler(const struct fota_download_evt *evt)
 			sm_fota_stage, sm_fota_status, sm_fota_info);
 		break;
 	case FOTA_DOWNLOAD_EVT_FINISHED:
+		/* fota_download is done; release the URI buffers. */
+		k_free(fota_hostname);
+		fota_hostname = NULL;
+		k_free(fota_path);
+		fota_path = NULL;
 		sm_fota_stage = FOTA_STAGE_ACTIVATE;
 		sm_fota_info = 0;
 		sm_modem_full_fota = (sm_fota_type == SM_FOTA_TYPE_FULL_MFW);
@@ -302,6 +325,11 @@ static void fota_dl_handler(const struct fota_download_evt *evt)
 		sm_fota_stage = FOTA_STAGE_INIT;
 		break;
 	case FOTA_DOWNLOAD_EVT_ERROR:
+		/* fota_download is done; release the URI buffers. */
+		k_free(fota_hostname);
+		fota_hostname = NULL;
+		k_free(fota_path);
+		fota_path = NULL;
 		sm_fota_status = FOTA_STATUS_ERROR;
 		sm_fota_info = evt->cause;
 		urc_send_to(fota_pipe, "\r\n#XFOTA: %d,%d,%d\r\n",
@@ -310,6 +338,11 @@ static void fota_dl_handler(const struct fota_download_evt *evt)
 		sm_fota_init_state();
 		break;
 	case FOTA_DOWNLOAD_EVT_CANCELLED:
+		/* fota_download is done; release the URI buffers. */
+		k_free(fota_hostname);
+		fota_hostname = NULL;
+		k_free(fota_path);
+		fota_path = NULL;
 		sm_fota_status = FOTA_STATUS_CANCELLED;
 		sm_fota_info = 0;
 		urc_send_to(fota_pipe, "\r\n#XFOTA: %d,%d\r\n", sm_fota_stage, sm_fota_status);