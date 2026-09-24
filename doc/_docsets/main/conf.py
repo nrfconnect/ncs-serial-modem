b768f4cecfaeb99461f6c2eda7362142d97e75bf
Tommi Rantanen <tommi.rantanen@nordicsemi.no>
2026-09-25 00:03:52 +0300

    doc: Restructuring nRF91M1 documentation
    Some tuning also to general SM doc.
    Further changes will follow but this is a starting point.
    The main changes are:
    - Added own toctree for nRF91M1 with AT commands and few other sections
    - Added nRF91M1 introduction.
    - Data mode documentation moved under AT commands
    - Data mode #XDATACTRL and #XDATAMODE removed from generic commands
    - PPP and nRF Provisioning order changed to alphabetical order
    - Added own page for AT syntax and content is from AT command intro
    
    Signed-off-by: Tommi Rantanen <tommi.rantanen@nordicsemi.no>
    (cherry picked from commit ed9e6a4951f5cb901987388d5cde9baef6d83125)

--- doc/_docsets/main/conf.py
+++ doc/_docsets/main/conf.py
@@ -15,11 +15,16 @@ from pathlib import Path
 sys.path.insert(0, str(Path(__file__).absolute().parents[1]))
 
 from conf_common import *  # noqa: F401,F403
+from conf_common import docsets, docset_exclude_patterns, html_theme_options
 
 DOCSET = "main"
 
-project, root_doc = docsets.ALL_DOCSETS[DOCSET]  # noqa: F405
+project, root_doc = docsets.ALL_DOCSETS[DOCSET]
 
-exclude_patterns = docset_exclude_patterns(DOCSET)  # noqa: F405
+exclude_patterns = docset_exclude_patterns(DOCSET) + [
+    # Pages that do not apply to the nRF91M1 go here.
+    "index_nrf91m1.rst",
+    "app/nrf91m1_intro.rst",
+]
 
-html_theme_options["docset"] = DOCSET  # noqa: F405
+html_theme_options["docset"] = DOCSET
